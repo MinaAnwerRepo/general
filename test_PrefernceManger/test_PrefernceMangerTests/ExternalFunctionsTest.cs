73433cf6d950c69cd9d204518ac7c45eb080aab1
MinaAnwer <31375784+MinaAnwerRepo@users.noreply.github.com>
2017-09-10 14:03:25 +0200

    add test_prefernece_project

---
+++ test_PrefernceManger/test_PrefernceMangerTests/ExternalFunctionsTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using test_PrefernceManger;
+
+namespace test_PrefernceMangerTests
+{
+    [TestClass()]
+  public  class ExternalFunctionsTest
+    {
+        [TestMethod()]
+        public void SerlializeToString_test()
+        {
+            customclass tstobj = new customclass() { name = "fddf", age = 12 };
+            int a = 32;
+            string str =   PreferanceManger.SerlializeToString(tstobj);
+            string str2 = PreferanceManger.SerlializeToString(a); 
+            
+        }
+
+
+    }
+ public class  customclass
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+   
+
+    }
+}
---
+++ test_PrefernceManger/test_PrefernceMangerTests1/PreferanceMangerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using test_PrefernceManger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_PrefernceManger.Tests
+{
+    [TestClass()]
+    public class PreferanceMangerTests
+    {
+        [TestMethod()]
+        public void SerlializeToStringTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
