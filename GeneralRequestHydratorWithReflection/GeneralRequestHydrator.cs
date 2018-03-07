c3315e8020b55c48974cedfee60876bdf5e64a53
MinaAnwer <31375784+MinaAnwerRepo@users.noreply.github.com>
2018-03-07 12:27:52 +0200

    add

---
+++ GeneralRequestHydratorWithReflection/GeneralRequestHydrator.cs
@@ -0,0 +1,44 @@
+﻿using MessageParsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Hydrators
+{
+ public   class GeneralRequestHydrator : IEntityHydrator<object>
+    {
+         public string Root { get; set; }
+         public object Obj;
+         public GeneralRequestHydrator( string root, object obj)
+        {
+            this.Root = root;
+            this.Obj = obj;
+        }
+        public object HydrateObject( Dictionary<string, string> kvp)
+        {
+            Type T = Obj.GetType();
+            PropertyInfo [] mainprop  = T.GetProperties();
+            object header = mainprop[0].GetValue(Obj);
+            object body = mainprop[1].GetValue(Obj);
+            Type Theader = header.GetType();
+            Type Tbody = body.GetType();
+            PropertyInfo[] headerpropes = Theader.GetProperties();
+            PropertyInfo[] bodypropes = Tbody.GetProperties();
+            foreach (var item in headerpropes)
+            {
+                item.SetValue(header, kvp[item.Name], null);
+            }
+            foreach (var item in bodypropes)
+            {
+                item.SetValue(body, kvp[item.Name], null);
+            }
+            return Obj;
+        }
+
+  
+    }
+
+}
---
+++ GeneralRequestHydratorWithReflection/GenrealRequestHyratorTests.cs
@@ -0,0 +1,29 @@
+﻿using DomainModel.BusinessObjects;
+using DomainModel.Hydrators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCMS.Tests
+{ 
+    [TestClass]
+  public  class generalhelpertest
+    {
+        [TestMethod]
+        public void GeneralRequestHydrator_with_InquireCustomerCardsRequest_test()
+        {
+            InquireCustomerCardsRequest reqobject = new InquireCustomerCardsRequest();
+            GeneralRequestHydrator req = new GeneralRequestHydrator("CMS.Inquire.Customer.Cards.Request/Data", reqobject);
+            Dictionary<string, string> kvp = new Dictionary<string, string>() ;
+            kvp["Version"] ="2.22" ;
+            kvp["ClientId"] = "2332323232322332";
+            kvp["RequestId"] = "rregergergewr";
+            kvp["Channel"] = "33232";
+            kvp["CustomerId"] = "tokojoet";
+            object obj=  req.HydrateObject( kvp);
+        }
+    }
+}
